9b5154180cf69d2f6e905e0cf7a6ac2930bfd787
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-10-12 23:13:34 +0600

    all done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis,Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis,Tooltip, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
      const allData = useLoaderData();
@@ -9,13 +9,16 @@ const Statistics = () => {
           <div>
                <h1 className='  my-5 text-2xl font-semibold'> No. of question line chart </h1>
                <div className=' flex justify-around my-5'>
-                    <LineChart width={500} height={300} data={Data}>
-                         <Line type="monotone" dataKey="total" stroke="#8884d8"> </Line>
-                         <XAxis dataKey="name" />
-                         <YAxis/>
-                         <CartesianGrid stroke="#ccc" />
-                         <Tooltip />
-                    </LineChart>
+                    <ResponsiveContainer width='70%' height={400}>
+                         <LineChart data={Data}>
+                              <Line type="monotone" dataKey="total" stroke="#8884d8"> </Line>
+                              <XAxis dataKey="name" />
+                              <YAxis/>
+                              <CartesianGrid stroke="#ccc" />
+                              <Tooltip />
+                              </LineChart>
+                    </ResponsiveContainer>
+                    
                </div>
           </div>
      );
